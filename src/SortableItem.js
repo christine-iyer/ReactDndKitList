abd3130f558d7b953b609de4a474404de80a8761
chrisIyer <hellohotbox420@gmail.com>
2024-03-07 14:12:33 -0500

    breaking it down

--- src/SortableItem.js
+++ src/SortableItem.js
@@ -25,3 +25,32 @@ export function SortableItem(props) {
         </div>
     )
 }
+
+
+// import { useSortable } from "@dnd-kit/sortable";
+// import {CSS} from "@dnd-kit/utilities";
+// import Card from 'react-bootstrap/Card';
+
+// export function SortableItem(props) {
+//     const { id, bkmk } = props;
+
+//     const {
+//         attributes,
+//         listeners,
+//         setNodeRef,
+//         transform,
+//         transition
+//     } = useSortable({id: props.id});
+
+//     const style = {
+//         transform: CSS.Transform.toString(transform),
+//         transition
+//     }
+
+//     return (
+//         <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+//             <Card.Title>{bkmk}</Card.Title>
+//             <Card body className="m-3" value={id}>{id}</Card>
+//         </div>
+//     )
+// }
